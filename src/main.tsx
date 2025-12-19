6e8315641e992dababeafe46d6f8ea8c233e190d
SAYEED <sayeedshirur@gmail.com>
2025-12-19 21:15:15 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,9 +14,9 @@ const queryClient = new QueryClient({
 })
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) {
-    return
-  }
+  // if (!import.meta.env.DEV) {
+  //   return
+  // }
   const { worker } = await import('./mocks/browser')
   return worker.start({
     onUnhandledRequest: 'bypass',
